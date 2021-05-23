fea33ad4f1b22375166503ba52d24c6e1469eac6
Anna Wästling <45712410+AnnaWastling@users.noreply.github.com>
2021-05-23 22:48:41 +0200

    flipcards

--- skurkeriet/src/app/skurkeriet/skurkeriet.component.ts
+++ skurkeriet/src/app/skurkeriet/skurkeriet.component.ts
@@ -1,57 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from "@angular/animations";
 
-export interface CardData {
-  imageId: string;
-  state: "default" | "flipped" | "matched";
-}
 @Component({
   selector: 'app-skurkeriet',
   templateUrl: './skurkeriet.component.html',
-  styleUrls: ['./skurkeriet.component.css'],
-  animations: [
-    trigger("cardFlip", [
-      state(
-        "default",
-        style({
-          transform: "none"
-        })
-      ),
-      state(
-        "flipped",
-        style({
-          transform: "rotateY(180deg)"
-        })
-      ),
-      state(
-        "matched",
-        style({
-          visibility: "false",
-          transform: "scale(0.05)",
-          opacity: 0
-        })
-      ),
-      transition("default => flipped", [animate("400ms")]),
-      transition("flipped => default", [animate("400ms")]),
-      transition("* => matched", [animate("400ms")])
-    ])
-  ]
+  styleUrls: ['./skurkeriet.component.css']
 })
 export class SkurkerietComponent implements OnInit {
-  data: CardData = {
-    imageId: "pDGNBK9A0sk",
-    state: "default"
-  };
-
   cols : number | undefined;
+  toggleProperty = false;
 
   gridByBreakpoint = {
     xl: 3,
@@ -90,11 +48,8 @@ export class SkurkerietComponent implements OnInit {
   }
   ngOnInit(): void {
   }
-  cardClicked() {
-    if (this.data.state === "default") {
-      this.data.state = "flipped";
-    } else {
-      this.data.state = "default";
-    }
+  toggle() {
+    this.toggleProperty = !this.toggleProperty;
   }
+
 }
