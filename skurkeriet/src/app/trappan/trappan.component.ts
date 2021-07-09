c75def28885ca3f93f02082b353b32ed0d6e7847
Anna Wästling <annwa917@student.liu.se>
2021-07-09 09:30:58 +0200

    fix

--- skurkeriet/src/app/trappan/trappan.component.ts
+++ skurkeriet/src/app/trappan/trappan.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-
+import {  Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-trappan',
   templateUrl: './trappan.component.html',
   styleUrls: ['./trappan.component.css']
 })
 export class TrappanComponent implements OnInit {
+  cols : number | undefined;
+  rowspan = 1;
+  colspan = 1;
+  gridByBreakpoint = {
+    xl: 3,
+    lg: 3,
+    md: 3,
+    sm: 3,
+    xs: 1
+  }
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver, private router:Router) {
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).subscribe(result => {
+      if (result.matches) {
+        if (result.breakpoints[Breakpoints.XSmall]) {
+          this.cols = this.gridByBreakpoint.xs;
+          this.rowspan = 2;
+          this.colspan = 1;
+        }
+        if (result.breakpoints[Breakpoints.Small]) {
+          this.cols = this.gridByBreakpoint.sm;
+          this.rowspan = 2;
+          this.colspan = 1;
 
-  ngOnInit(): void {
+        }
+        if (result.breakpoints[Breakpoints.Medium]) {
+          this.cols = this.gridByBreakpoint.md;
+          this.rowspan = 2;
+          this.colspan = 1;
+
+        }
+        if (result.breakpoints[Breakpoints.Large]) {
+          this.cols = this.gridByBreakpoint.lg;
+          this.rowspan = 2;
+          this.colspan = 1;
+
+        }
+        if (result.breakpoints[Breakpoints.XLarge]) {
+          this.cols = this.gridByBreakpoint.xl;
+          this.rowspan = 2;
+          this.colspan = 1;
+
+        }
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  navigate(){
+    
   }
 
 }
